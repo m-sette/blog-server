c71a437b8c62fe2b996b454a32d8f865ce33b088
Matteo Sette <msette8@gmail.com>
2020-04-28 11:01:03 +0200

    implemented route, model, controller

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-const express = require("express");
-const app = express();
-const db = require("./utils/db");
+const express = require('express')
+const app = express()
+const api = require('./api')
 
-app.set("port", process.env.PORT || 8081);
+app.set('port', process.env.PORT || 8081)
 
-app.use(express.static("static"));
+app.use('/api', api)
+app.use(express.static('static'))
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
 
-app.get("/user", async (req, res) => {
-  let email = "j@d.com";
-  try {
-    let { rows } = await db.getUser(email);
-    res.status(200).json(rows);
-  } catch (e) {
-    console.log("Error on the get user route", e);
-  }
-});
-
-app.listen(app.get("port"), () =>
-  console.log(`API Server Listening on port " ${app.get("port")}`)
-);
+app.listen(app.get('port'), () =>
+  console.log(`API Server Listening on port " ${app.get('port')}`)
+)
